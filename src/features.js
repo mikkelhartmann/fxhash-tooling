3a3c4f1158268bd38aa65255f3a04c852244b5b3
Mads Hartmann <mads379@gmail.com>
2022-08-20 19:35:17 +0000

    Format and minor cleanup

--- src/features.js
+++ src/features.js
@@ -1,58 +1,52 @@
+function getShape() {
+  return random(["circle", "square"]); //
+}
+
+function getBackgroundColor() {
+  const colors = [
+    {
+      name: "1",
+      background: "#4F4978",
+      fill: "#963721",
+      stroke: "#BE8238",
+    },
+    {
+      name: "2",
+      background: "#BF5934",
+      fill: "#45413B",
+      stroke: "#B2C47A",
+    },
+    {
+      name: "3",
+      background: "#B2C47A",
+      fill: "#DFE6E8",
+      stroke: "#C9A56B",
+    },
+    {
+      name: "4",
+      background: "#BE8238",
+      fill: "#E5C846",
+      stroke: "#EDC2DD",
+    },
+    {
+      name: "4",
+      background: "#F0941D",
+      fill: "#F0C651",
+      stroke: "#5BA69E",
+    },
+  ];
+  return random(colors);
+}
+
 export function getFeatures(fxrand) {
+  const features = {
+    palette: getBackgroundColor(),
+    shape: getShape(),
+  };
 
+  for (const [key, value] of Object.entries(features)) {
+    console.log(key, value);
+  }
 
-    let getShape = function () {
-        return random(['circle', 'square']) //
-
-    }
-
-    let getBackgroundColor = function () {
-
-        const colors = [
-            {
-                'name': '1',
-                'background': '#4F4978',
-                'fill': '#963721',
-                'stroke': '#BE8238'
-            },
-            {
-                'name': '2',
-                'background': '#BF5934',
-                'fill': '#45413B',
-                'stroke': '#B2C47A'
-            },
-            {
-                'name': '3',
-                'background': '#B2C47A',
-                'fill': '#DFE6E8',
-                'stroke': '#C9A56B'
-            },
-            {
-                'name': '4',
-                'background': '#BE8238',
-                'fill': '#E5C846',
-                'stroke': '#EDC2DD'
-            },
-            {
-                'name': '4',
-                'background': '#F0941D',
-                'fill': '#F0C651',
-                'stroke': '#5BA69E'
-            }
-
-        ]
-        return random(colors)
-    }
-
-    const features = {
-        'palette': getBackgroundColor(),
-        'shape': getShape(),
-    }
-
-    for (const [key, value] of Object.entries(features)) {
-        console.log(key, value);
-    }
-
-    return features
-
+  return features;
 }
--- src/index.js
+++ src/index.js
@@ -1,52 +1,40 @@
 // By importing p5 we make all the global p5 functions available
-import 'p5'
-import { getFeatures } from './features'
+import "p5";
+import { getFeatures } from "./features";
 
 function setup() {
-    let features = getFeatures(fxrand)
+  const features = getFeatures(fxrand);
 
-    window.$fxhashFeatures = {
-        "palette": features.palette['name'],
-        "shape": features.shape
-    }
+  window.$fxhashFeatures = {
+    palette: features.palette.name,
+    shape: features.shape,
+  };
 
-    let width = 3400
-    let height = 3400
+  const width = 3400;
+  const height = 3400;
 
-    createCanvas(width, height)
-    const pg = createGraphics(width, height);
-    background(color(features.palette['background']));
+  createCanvas(width, height);
+  background(color(features.palette["background"]));
+  push();
+  fill(features.palette["fill"]);
+  stroke(features.palette["stroke"]);
+  strokeWeight(width / 4);
 
-    push()
-    fill(features.palette['fill'])
-    stroke(features.palette['stroke'])
-    strokeWeight(width / 4)
+  switch (features.shape) {
+    case "circle":
+      circle(width / 2, height / 2, width / 2);
+      break;
 
-    switch (features.shape) {
-        case 'circle':
-            circle(width / 2, height / 2, width / 2)
-            break;
-
-        case 'square':
-            rectMode(CENTER)
-            rect(width / 2, height / 2, width / 2, width / 2)
-            break;
-    }
-
-
-
-    pop()
-
-    image(
-        pg,
-        0.033 * width,
-        0.0227 * height,
-        0.935 * width,
-        0.954 * height
-    )
+    case "square":
+      rectMode(CENTER);
+      rect(width / 2, height / 2, width / 2, width / 2);
+      break;
+  }
 }
 
-function draw() { noLoop() }
+function draw() {
+  noLoop();
+}
 
 // Make the functions available to p5
 window.setup = setup;
