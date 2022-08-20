29d2796c9001fbc2d31eb8531faa7d0a933a67de
Mads Hartmann <mads379@gmail.com>
2022-08-20 12:00:18 +0000

    Remove generation of main element
    It appears that p5 creates it for us anyway

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 // By importing p5 we make all the global p5 functions available
 import 'p5'
 
-const container = document.createElement("main");
-container.setAttribute("id", "main");
-document.body.prepend(container);
-
 var script = document.createElement('script');
 script.type = 'text/javascript';
 script.src = './script.js';
